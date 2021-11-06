c7a9bbaa9c3083ef7c9c1082dffb2b0e174637f8
CaioGiasson <cainitech@gmail.com>
2021-11-06 17:49:09 -0300

    Múltiplos personagens, movendo somente o selecionado

--- novoJogo/game.js
+++ novoJogo/game.js
@@ -1,22 +1,52 @@
-var personagem = document.getElementById("personagem")
-var posX = 100
-var posY = 100
+// SELECIONANDO UM PERSONAGEM
+function selecionar (element) {
+  // DES-selecionando o selecionado atual
+  var selecionado = document.getElementsByClassName("selecionado")[0]
+  if (selecionado) {
+    selecionado.classList.remove("selecionado")
+  }
+
+  // SELECIONANDO O NOVO PERSONAGEM
+  element.classList.add("selecionado")
+
+  // ATUALIZANDO A POSIÇÃO DO PERSONAGEM
+  clickX = element.style.left - largura / 2
+  clickY = element.style.top - altura / 2
+
+}
+
+
+// ALEATORIZANDO A POSIÇÃO DE UM PERSONAGEM ESPECÍFICO
+function posicionar (personagem) {
+  var px = 100 + Math.random() * 500
+  var py = 100 + Math.random() * 500
+  personagem.style.left = px + 'px'
+  personagem.style.top = py + 'px'
+}
+
+// COLETANDO A LISTA DE PERSONAGENS DA TELA E ALEATORIZANDO A POSIÇÃO DE CADA UM
+var todosPersonagens = document.getElementsByClassName('personagem')
+for (personagem of todosPersonagens) {
+  posicionar(personagem)
+}
+
+var personagemSelecionado = todosPersonagens[0]
+selecionar(personagemSelecionado)
+var posX, posY
 var largura = 50
 var altura = 50
 var clickX = posX
 var clickY = posY
 var velocidade = 5
 
-console.log("Antes: " + document.getElementById("personagem").style.top)
-
-personagem.style.left = posX + "px"
-personagem.style.top = posY + "px"
-personagem.style.width = largura + "px"
-personagem.style.height = altura + "px"
-
-console.log("Depois: " + document.getElementById("personagem").style.top)
+personagemSelecionado.style.width = largura + "px"
+personagemSelecionado.style.height = altura + "px"
 
 setInterval(function () {
+  personagemSelecionado = document.getElementsByClassName("selecionado")[0]
+  posX = parseInt(personagemSelecionado.style.left)
+  posY = parseInt(personagemSelecionado.style.top)
+
   var deltaX = clickX - posX
   var deltaY = clickY - posY
 
@@ -27,14 +57,17 @@ setInterval(function () {
   posX += velocidade * Math.cos(angulo)
   posY += velocidade * Math.sin(angulo)
 
-  personagem.style.left = posX + "px"
-  personagem.style.top = posY + "px"
+  personagemSelecionado.style.left = posX + "px"
+  personagemSelecionado.style.top = posY + "px"
 }, 20)
 
 function capturarPosicaoMouse (event) {
-  console.log(personagem.style.width)
-  clickX = event.clientX - largura / 2
-  clickY = event.clientY - altura / 2
+  var clicouEmPersonagem = event.target.classList.contains("personagem")
+
+  if (!clicouEmPersonagem) {
+    clickX = event.clientX - largura / 2
+    clickY = event.clientY - altura / 2
+  }
 }
 document.addEventListener("mousedown", capturarPosicaoMouse)
 
